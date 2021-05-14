ffe088c8b760d729fb58d115264a1d2c2641a39e
dinesh <chavandinesh53@gmail.com>
2021-05-15 02:53:25 +0530

    added changes again with disable logs

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Context} from './src/context/Context';
+import {LogBox} from 'react-native'
 import Routes from './src/routes/routes';
 export default function App() {
+  LogBox.ignoreAllLogs()
   return (
     <Context>
       <Routes />
